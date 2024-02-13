315dbf3abe3f186c66e5e505944233f699ade105
Akshat <iamakku0.0.0.1@gmail.com>
2024-02-13 21:50:46 +0530

    dynamic routing

---
+++ src/app/products/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const products = () => {
+  return (
+    <>
+
+    <div>product 1</div>
+    <div>product 2</div>
+    <div>product 3</div>
+
+    </>
+   
+  )
+}
+
+export default products
