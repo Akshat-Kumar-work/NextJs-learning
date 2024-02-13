ab7e63a80df495c7b2e36f33177175cd7a31f127
Akshat <iamakku0.0.0.1@gmail.com>
2024-02-13 23:35:16 +0530

    nested Layouts

---
+++ src/app/products/[productId]/layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProductDetailsLayout = ({children}) => {
+  return (
+    <div>
+        <h2>Features Product from Product id layout</h2>
+        {children}
+
+    </div>
+  )
+}
+
+export default ProductDetailsLayout
