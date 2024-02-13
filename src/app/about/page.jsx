b786ff2c8b8a152daad94ed8f0907bace1920db8
Akshat <iamakku0.0.0.1@gmail.com>
2024-02-14 00:13:22 +0530

    setting meta data dynamically

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+export const metadata = {
+  title:"About Akshat",
+  descrition:"Akshat is very smart"
+}
+
 const About = () => {
   return (
     <div>about page</div>
--- src/app/products/[productId]/page.jsx
+++ src/app/products/[productId]/page.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+//setting title dynamically
+export const generateMetadata = ({params})=>{
+  return {
+    title: `Product ${params.productId}`
+  }
+}
+
 const productId = ({params}) => {
   return (
     <div>productId {params.productId}</div>
