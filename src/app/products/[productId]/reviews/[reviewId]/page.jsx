dfd7fdfed544b012de11ef8963609eaacd2d20e9
Akshat <iamakku0.0.0.1@gmail.com>
2024-02-14 09:26:10 +0530

    custom error handling

--- src/app/products/[productId]/reviews/[reviewId]/page.jsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.jsx
@@ -1,10 +1,20 @@
+
 import { notFound } from 'next/navigation';
+
 
 import React from 'react'
 
 const reviewId = ({params}) => {
+
+
+
   if(parseInt(params.reviewId)>100){
     return notFound();
+  }
+
+  const random = 2;
+  if(random ===1){
+    throw new Error("Error loading review")
   }
   return (
     <h1>
