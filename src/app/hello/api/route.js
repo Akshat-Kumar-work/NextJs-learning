349acbf66e7026832b12ca10e275439dbda871bb
Akshat <iamakku0.0.0.1@gmail.com>
2024-02-14 15:38:26 +0530

    route handler

---
+++ src/app/hello/api/route.js
@@ -0,0 +1,31 @@
+
+//get route handler for hello route
+export async function GET(request) {
+    const { searchParams } = new URL(request.url)
+    const id = searchParams.get('id')
+    const res = await fetch(`https://data.mongodb-api.com/product/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'API-Key': process.env.DATA_API_KEY,
+      },
+    })
+    const product = await res.json()
+   
+    return Response.json({ product })
+  }
+
+//post route hanlder for hello route
+export async function POST() {
+    const res = await fetch('https://data.mongodb-api.com/...', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'API-Key': process.env.DATA_API_KEY,
+      },
+      body: JSON.stringify({ time: new Date().toISOString() }),
+    })
+   
+    const data = await res.json()
+   
+    return Response.json( data)
+  }
